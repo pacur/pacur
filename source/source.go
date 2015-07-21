6ea583d3a38b2352aea05e5c2aa04b0da8f705b8
Zachary Huff <zach.huff.386@gmail.com>
2015-07-21 19:59:08 -0400

    Add get url to source

--- source/source.go
+++ source/source.go
@@ -1,9 +1,51 @@
 package source
 
+import (
+	"github.com/pacur/pacur/utils"
+	"strings"
+)
+
+const (
+	path = 0
+	url  = 1
+)
+
 type Source struct {
-	Path string
+	Path   string
+	Output string
 }
 
-func (s *Source) Get(dir string) (err error) {
+func (s *Source) getType() int {
+	if strings.HasPrefix(s.Path, "http") {
+		return url
+	}
+	return path
+}
+
+func (s *Source) getUrl() (err error) {
+	err = utils.HttpGet(s.Path, s.Output)
+	if err != nil {
+		return
+	}
+	return
+}
+
+func (s *Source) getPath() (err error) {
+	return
+}
+
+func (s *Source) Get() (err error) {
+	switch s.getType() {
+	case url:
+		err = s.getUrl()
+	case path:
+		err = s.getPath()
+	default:
+		panic("utils: Unknown type")
+	}
+	if err != nil {
+		return
+	}
+
 	return
 }
