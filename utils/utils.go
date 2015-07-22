134d8f974b877d8190443f4848e9073ffad416ad
Zachary Huff <zach.huff.386@gmail.com>
2015-07-21 20:07:22 -0400

    Fix dir permissions in utils

--- utils/utils.go
+++ utils/utils.go
@@ -19,7 +19,7 @@ func UrlFilename(url string) string {
 func ExistsMakeDir(path string) (err error) {
 	_, err = os.Stat(path)
 	if os.IsNotExist(err) {
-		err = os.Mkdir(path, 755)
+		err = os.Mkdir(path, 0755)
 		if err != nil {
 			err = &MakeDirError{
 				errors.Wrap(err, "utils: Failed to stat dir"),
