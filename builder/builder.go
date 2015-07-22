888df04f08792fea64086f5a1afab3fc17c2cf5d
Zachary Huff <zach.huff.386@gmail.com>
2015-07-22 02:17:49 -0400

    Add build to builder

--- builder/builder.go
+++ builder/builder.go
@@ -1,12 +1,18 @@
 package builder
 
 import (
+	"fmt"
+	"github.com/dropbox/godropbox/errors"
 	"github.com/pacur/pacur/pack"
 	"github.com/pacur/pacur/source"
 	"github.com/pacur/pacur/utils"
+	"os"
+	"os/exec"
+	"path/filepath"
 )
 
 type Builder struct {
+	id   string
 	Pack *pack.Pack
 }
 
@@ -40,13 +46,55 @@ func (b *Builder) getSources() (err error) {
 	return
 }
 
+func (b *Builder) build() (err error) {
+	path := filepath.Join(string(os.PathSeparator), "tmp",
+		fmt.Sprintf("pacur_%s_build", b.id))
+
+	script, err := os.Create(path)
+	if err != nil {
+		err = &BuilderError{
+			errors.Wrap(err, "builder: Failed to create build script"),
+		}
+		return
+	}
+	defer func() {
+		os.Remove(path)
+	}()
+
+	for _, cmd := range b.Pack.Build {
+		script.WriteString(cmd + "\n")
+	}
+
+	cmd := exec.Command("sh", path)
+	cmd.Dir = b.Pack.SourceDir
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err = cmd.Run()
+	if err != nil {
+		err = &BuilderError{
+			errors.Wrapf(err, "builder: Failed to exec build script"),
+		}
+		return
+	}
+
+	return
+}
+
 func (b *Builder) Build() (err error) {
+	b.id = utils.RandStr(12)
+
 	err = b.initDirs()
 	if err != nil {
 		return
 	}
 
 	err = b.getSources()
+	if err != nil {
+		return
+	}
+
+	err = b.build()
 	if err != nil {
 		return
 	}
