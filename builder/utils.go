41c09d9ee52ec34096e2361d2770d74633fb887a
Zachary Huff <zach.huff.386@gmail.com>
2015-07-22 04:25:49 -0400

    Add utils to builder

---
+++ builder/utils.go
@@ -0,0 +1,50 @@
+package builder
+
+import (
+	"github.com/dropbox/godropbox/errors"
+	"os"
+	"os/exec"
+)
+
+func createScript(path string, cmds []string) (err error) {
+	script, err := os.Create(path)
+	if err != nil {
+		err = &BuilderError{
+			errors.Wrapf(err, "builder: Failed to create script '%s'", path),
+		}
+		return
+	}
+	defer func() {
+		script.Close()
+	}()
+
+	for _, cmd := range cmds {
+		_, err = script.WriteString(cmd + "\n")
+		if err != nil {
+			err = &BuilderError{
+				errors.Wrapf(err, "builder: Failed to write script '%s'",
+					path),
+			}
+			return
+		}
+	}
+
+	return
+}
+
+func runScript(path, dir string) (err error) {
+	cmd := exec.Command("sh", path)
+	cmd.Dir = dir
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err = cmd.Run()
+	if err != nil {
+		err = &BuilderError{
+			errors.Wrapf(err, "builder: Failed to exec script"),
+		}
+		return
+	}
+
+	return
+}
