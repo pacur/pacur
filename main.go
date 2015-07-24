24501d0ba2cf44fba1d222d579cf90d0fd2b3ca2
Zachary Huff <zach.huff.386@gmail.com>
2015-07-24 03:50:35 -0400

    Add flags to main

--- main.go
+++ main.go
@@ -4,9 +4,33 @@ import (
 	"github.com/pacur/pacur/builder"
 	"github.com/pacur/pacur/debian"
 	"github.com/pacur/pacur/parse"
+	"flag"
 )
 
 func main() {
+	flag.Parse()
+
+	distro := ""
+	release := ""
+
+	arg := flag.Arg(0)
+	switch arg {
+	case "ubuntu-precise":
+		distro = "ubuntu"
+		release = "precise"
+	case "ubuntu-trusty":
+		distro = "ubuntu"
+		release = "trusty"
+	case "ubuntu-vivid":
+		distro = "ubuntu"
+		release = "vivid"
+	case "ubuntu-wily":
+		distro = "ubuntu"
+		release = "wily"
+	default:
+		panic("main: Unknown build target " + arg)
+	}
+
 	pac, err := parse.File("/pacur/PKGBUILD")
 	if err != nil {
 		panic(err)
@@ -25,11 +49,17 @@ func main() {
 		panic(err)
 	}
 
-	deb := debian.Debian{
-		Pack: pac,
-	}
-	err = deb.Build()
-	if err != nil {
-		panic(err)
+	switch distro {
+	case "ubuntu":
+		deb := debian.Debian{
+			Pack: pac,
+			Release: release,
+		}
+		err = deb.Build()
+		if err != nil {
+			panic(err)
+		}
+	default:
+		panic("main: Unknown distro")
 	}
 }
