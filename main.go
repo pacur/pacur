d36fc77f2f2e41a53d6ae326cdef108321907d9c
Zachary Huff <zach.huff.386@gmail.com>
2015-07-24 06:23:08 -0400

    Run prep in main

--- main.go
+++ main.go
@@ -41,6 +41,20 @@ func main() {
 		panic(err)
 	}
 
+	if distro != "ubuntu" {
+		panic("main: Unknown distro")
+	}
+
+	deb := debian.Debian{
+		Pack:    pac,
+		Release: release,
+	}
+
+	err = deb.Prep()
+	if err != nil {
+		panic(err)
+	}
+
 	builder := builder.Builder{
 		Pack: pac,
 	}
@@ -49,17 +63,22 @@ func main() {
 		panic(err)
 	}
 
-	switch distro {
-	case "ubuntu":
-		deb := debian.Debian{
-			Pack:    pac,
-			Release: release,
-		}
-		err = deb.Build()
-		if err != nil {
-			panic(err)
-		}
-	default:
-		panic("main: Unknown distro")
+	err = deb.Build()
+	if err != nil {
+		panic(err)
 	}
+
+//	switch distro {
+//	case "ubuntu":
+//		deb := debian.Debian{
+//			Pack:    pac,
+//			Release: release,
+//		}
+//		err = deb.Build()
+//		if err != nil {
+//			panic(err)
+//		}
+//	default:
+//		panic("main: Unknown distro")
+//	}
 }
