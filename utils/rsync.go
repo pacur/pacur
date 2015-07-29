8c5fdcd2f0cc593c4801fe048e82afad55048695
Zachary Huff <zach.huff.386@gmail.com>
2015-07-29 03:44:59 -0400

    Add rsync utils

---
+++ utils/rsync.go
@@ -0,0 +1,46 @@
+package utils
+
+import (
+	"github.com/dropbox/godropbox/errors"
+	"os"
+	"os/exec"
+)
+
+func Rsync(source, dest string) (err error) {
+	cmd := exec.Command("rsync", "-a", "-A", "-X",
+		source+string(os.PathSeparator),
+		dest+string(os.PathSeparator))
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err = cmd.Run()
+	if err != nil {
+		err = &CopyError{
+			errors.Wrapf(err, "utils: Failed to rsync '%s' to '%s'", source,
+				dest),
+		}
+		return
+	}
+
+	return
+}
+
+func RsyncExt(source, dest, ext string) (err error) {
+	cmd := exec.Command("rsync", "-a", "-A", "-X",
+		"--include", "*." + ext, "--exclude", "*",
+		source+string(os.PathSeparator),
+		dest+string(os.PathSeparator))
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err = cmd.Run()
+	if err != nil {
+		err = &CopyError{
+			errors.Wrapf(err, "utils: Failed to rsync '%s' to '%s'", source,
+				dest),
+		}
+		return
+	}
+
+	return
+}
