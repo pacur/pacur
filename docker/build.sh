50c4f31839cafe4fa433c9b55534e032ede193f0
Zachary Huff <zach.huff.386@gmail.com>
2015-07-29 20:32:35 -0400

    Use date in docker build

--- docker/build.sh
+++ docker/build.sh
@@ -2,33 +2,39 @@
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 cd centos-6
-docker build --rm --no-cache -t centos-6 .&
+sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur #`date`|g" Dockerfile
+docker build --rm -t centos-6 .
+sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur|g" Dockerfile
 cd ..
 
 cd centos-7
-docker build --rm --no-cache -t centos-7 .&
+sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur #`date`|g" Dockerfile
+docker build --rm -t centos-7 .
+sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur|g" Dockerfile
 cd ..
 
 cd debian-jessie
-docker build --rm --no-cache -t debian-jessie .&
+sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur #`date`|g" Dockerfile
+docker build --rm -t debian-jessie .
+sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur|g" Dockerfile
 cd ..
 
 cd debian-wheezy
-docker build --rm --no-cache -t debian-wheezy .&
+sh -c 'sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur #`date`|g" Dockerfile; docker build --rm -t debian-wheezy .; sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur|g" Dockerfile' &
 cd ..
 
 cd ubuntu-precise
-docker build --rm --no-cache -t ubuntu-precise .&
+sh -c'sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur #`date`|g" Dockerfile; docker build --rm -t ubuntu-precise .; sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur|g" Dockerfile' &
 cd ..
 
 cd ubuntu-trusty
-docker build --rm --no-cache -t ubuntu-trusty .&
+sh -c'sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur #`date`|g" Dockerfile; docker build --rm -t ubuntu-trusty .; sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur|g" Dockerfile' &
 cd ..
 
 cd ubuntu-vivid
-docker build --rm --no-cache -t ubuntu-vivid .&
+sh -c'sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur #`date`|g" Dockerfile; docker build --rm -t ubuntu-vivid .; sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur|g" Dockerfile' &
 cd ..
 
 cd ubuntu-wily
-docker build --rm --no-cache -t ubuntu-wily .&
+sh -c'sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur #`date`|g" Dockerfile; docker build --rm -t ubuntu-wily .; sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur|g" Dockerfile' &
 cd ..
