948fea91d7797a89c80252b5a2e64097902531a9
Zachary Huff <zach.huff.386@gmail.com>
2015-07-29 20:16:03 -0400

    Improve docker build

--- docker/build.sh
+++ docker/build.sh
@@ -2,33 +2,33 @@
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 cd centos-6
-docker build --rm -t centos-6 .
+docker build --rm --no-cache -t centos-6 .&
 cd ..
 
 cd centos-7
-docker build --rm -t centos-7 .
+docker build --rm --no-cache -t centos-7 .&
 cd ..
 
 cd debian-jessie
-docker build --rm -t debian-jessie .
+docker build --rm --no-cache -t debian-jessie .&
 cd ..
 
 cd debian-wheezy
-docker build --rm -t debian-wheezy .
+docker build --rm --no-cache -t debian-wheezy .&
 cd ..
 
 cd ubuntu-precise
-docker build --rm -t ubuntu-precise .
+docker build --rm --no-cache -t ubuntu-precise .&
 cd ..
 
 cd ubuntu-trusty
-docker build --rm -t ubuntu-trusty .
+docker build --rm --no-cache -t ubuntu-trusty .&
 cd ..
 
 cd ubuntu-vivid
-docker build --rm -t ubuntu-vivid .
+docker build --rm --no-cache -t ubuntu-vivid .&
 cd ..
 
 cd ubuntu-wily
-docker build --rm -t ubuntu-wily .
+docker build --rm --no-cache -t ubuntu-wily .&
 cd ..
