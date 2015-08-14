ad59ce131b6852ca5279921b97f0799483cef5aa
Zachary Huff <zach.huff.386@gmail.com>
2015-08-14 17:08:08 -0400

    Add arch prep

--- arch/project.go
+++ arch/project.go
@@ -7,32 +7,54 @@ import (
 )
 
 type ArchProject struct {
-	Root    string
-	Path    string
-	Distro  string
-	Release string
+	Root       string
+	MirrorRoot string
+	BuildRoot  string
+	Path       string
+	Distro     string
+	Release    string
+}
+
+func (p *ArchProject) getBuildDir() (path string, err error) {
+	path = filepath.Join(p.BuildRoot, p.Distro)
+
+	err = utils.MkdirAll(path)
+	if err != nil {
+		return
+	}
+
+	return
 }
 
 func (p *ArchProject) Prep() (err error) {
+	buildDir, err := p.getBuildDir()
+	if err != nil {
+		return
+	}
+
+	err = utils.RsyncExt(p.Path, buildDir, ".pkg.tar.xz")
+	if err != nil {
+		return
+	}
+
 	return
 }
 
 func (p *ArchProject) Create() (err error) {
-	archDir := filepath.Join(p.Path, "arch")
+	buildDir, err := p.getBuildDir()
+	if err != nil {
+		return
+	}
 
 	err = utils.Exec("", "docker", "run", "--rm", "-t", "-v",
-		p.Path+":/pacur", constants.DockerOrg+p.Distro, "create",
+		buildDir+":/pacur", constants.DockerOrg+p.Distro, "create",
 		p.Distro)
 	if err != nil {
 		return
 	}
 
-	err = utils.Rsync(archDir, filepath.Join(p.Root, "mirror", "arch"))
-	if err != nil {
-		return
-	}
-
-	err = utils.RemoveAll(archDir)
+	err = utils.Rsync(filepath.Join(buildDir, "arch"),
+		filepath.Join(p.MirrorRoot, "arch"))
 	if err != nil {
 		return
 	}
