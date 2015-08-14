31f11a0afba964752a1879d56adcf2159731c0ef
Zachary Huff <zach.huff.386@gmail.com>
2015-08-14 11:59:38 -0400

    Add releases to constants

--- constants/constants.go
+++ constants/constants.go
@@ -3,3 +3,16 @@ package constants
 const (
 	DockerOrg = "pacur/"
 )
+
+var (
+	Releases = [...]string{
+		"archlinux",
+		"centos-7",
+		"debian-jessie",
+		"debian-wheezy",
+		"ubuntu-precise",
+		"ubuntu-trusty",
+		"ubuntu-vivid",
+		"ubuntu-wily",
+	}
+)
