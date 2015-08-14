27ba3c1276696953b8d7a43eddcfceea5c37cdaf
Zachary Huff <zach.huff.386@gmail.com>
2015-08-14 17:08:36 -0400

    Add redhat prep

--- redhat/project.go
+++ redhat/project.go
@@ -7,32 +7,54 @@ import (
 )
 
 type RedhatProject struct {
-	Root    string
-	Path    string
-	Distro  string
-	Release string
+	Root       string
+	MirrorRoot string
+	BuildRoot  string
+	Path       string
+	Distro     string
+	Release    string
+}
+
+func (p *RedhatProject) getBuildDir() (path string, err error) {
+	path = filepath.Join(p.BuildRoot, p.Distro)
+
+	err = utils.MkdirAll(path)
+	if err != nil {
+		return
+	}
+
+	return
 }
 
 func (p *RedhatProject) Prep() (err error) {
+	buildDir, err := p.getBuildDir()
+	if err != nil {
+		return
+	}
+
+	err = utils.RsyncExt(p.Path, buildDir, ".rpm")
+	if err != nil {
+		return
+	}
+
 	return
 }
 
 func (p *RedhatProject) Create() (err error) {
-	yumDir := filepath.Join(p.Path, "yum")
+	buildDir, err := p.getBuildDir()
+	if err != nil {
+		return
+	}
 
 	err = utils.Exec("", "docker", "run", "--rm", "-t", "-v",
-		p.Path+":/pacur", constants.DockerOrg+p.Distro+"-"+p.Release,
+		buildDir+":/pacur", constants.DockerOrg+p.Distro+"-"+p.Release,
 		"create", p.Distro+"-"+p.Release)
 	if err != nil {
 		return
 	}
 
-	err = utils.Rsync(yumDir, filepath.Join(p.Root, "mirror", "yum"))
-	if err != nil {
-		return
-	}
-
-	err = utils.RemoveAll(yumDir)
+	err = utils.Rsync(filepath.Join(buildDir, "yum"),
+		filepath.Join(p.MirrorRoot, "yum"))
 	if err != nil {
 		return
 	}
