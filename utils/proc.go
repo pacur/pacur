faae7c33fc7db4ec0ae1f88cb0d0d157cd97e4c0
Zachary Huff <zach.huff.386@gmail.com>
2015-08-19 02:24:01 -0400

    Add exec input to utils

--- utils/proc.go
+++ utils/proc.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"github.com/dropbox/godropbox/errors"
+	"io"
 	"os"
 	"os/exec"
 )
@@ -16,6 +17,51 @@ func Exec(dir, name string, arg ...string) (err error) {
 	}
 
 	err = cmd.Run()
+	if err != nil {
+		err = &ExecError{
+			errors.Wrapf(err, "utils: Failed to exec '%s'", name),
+		}
+		return
+	}
+
+	return
+}
+
+func ExecInput(dir, input, name string, arg ...string) (err error) {
+	cmd := exec.Command(name, arg...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		err = &ExecError{
+			errors.Wrapf(err, "utils: Failed to get stdin in exec '%s'", name),
+		}
+		return
+	}
+	defer stdin.Close()
+
+	if dir != "" {
+		cmd.Dir = dir
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		err = &ExecError{
+			errors.Wrapf(err, "utils: Failed to exec '%s'", name),
+		}
+		return
+	}
+	_, err = io.WriteString(stdin, input)
+	if err != nil {
+		err = &ExecError{
+			errors.Wrapf(err, "utils: Failed to write stdin in exec '%s'",
+				name),
+		}
+		return
+	}
+
+	err = cmd.Wait()
 	if err != nil {
 		err = &ExecError{
 			errors.Wrapf(err, "utils: Failed to exec '%s'", name),
