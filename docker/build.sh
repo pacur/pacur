267ddcc6c0dfc04a18e59ee86ca0850ec09e4d80
Zachary Huff <zach.huff.386@gmail.com>
2015-08-22 03:06:15 -0400

    Use loop in docker scripts

--- docker/build.sh
+++ docker/build.sh
@@ -1,62 +1,10 @@
 #!/bin/bash
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-cd archlinux
-sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur #`date`|g" Dockerfile
-docker build --rm -t archlinux .
-sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur|g" Dockerfile
-cd ..
-
-cd centos-6
-sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur #`date`|g" Dockerfile
-docker build --rm -t centos-6 .
-sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur|g" Dockerfile
-cd ..
-
-cd centos-7
-sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur #`date`|g" Dockerfile
-docker build --rm -t centos-7 .
-sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur|g" Dockerfile
-cd ..
-
-cd debian-jessie
-sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur #`date`|g" Dockerfile
-docker build --rm -t debian-jessie .
-sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur|g" Dockerfile
-cd ..
-
-cd debian-wheezy
-sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur #`date`|g" Dockerfile
-docker build --rm -t debian-wheezy .
-sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur|g" Dockerfile
-cd ..
-
-cd ubuntu-precise
-sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur #`date`|g" Dockerfile
-docker build --rm -t ubuntu-precise .
-sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur|g" Dockerfile
-cd ..
-
-cd ubuntu-trusty
-sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur #`date`|g" Dockerfile
-docker build --rm -t ubuntu-trusty .
-sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur|g" Dockerfile
-cd ..
-
-cd ubuntu-vivid
-sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur #`date`|g" Dockerfile
-docker build --rm -t ubuntu-vivid .
-sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur|g" Dockerfile
-cd ..
-
-cd ubuntu-wily
-sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur #`date`|g" Dockerfile
-docker build --rm -t ubuntu-wily .
-sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur|g" Dockerfile
-cd ..
-
-cd genkey
-sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur #`date`|g" Dockerfile
-docker build --rm -t genkey .
-sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur|g" Dockerfile
-cd ..
+for dir in */ ; do
+    cd $dir
+    sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur #`date`|g" Dockerfile
+    docker build --rm -t ${dir::-1} .
+    sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur|g" Dockerfile
+    cd ..
+done
