c3b26a9b29022322b66dcb7d7fac0fe9f454b4fc
Zachary Huff <zach.huff.386@gmail.com>
2015-09-06 05:07:31 -0400

    Add directives to constants

--- constants/constants.go
+++ constants/constants.go
@@ -1,5 +1,10 @@
 package constants
 
+import (
+	"github.com/dropbox/godropbox/container/set"
+	"strings"
+)
+
 const (
 	DockerOrg = "pacur/"
 )
@@ -24,4 +29,40 @@ var (
 		"debian":    "debian",
 		"ubuntu":    "debian",
 	}
+	Packagers = [...]string{
+		"apt",
+		"pacman",
+		"yum",
+	}
+
+	ReleasesSet    = set.NewSet(Releases)
+	Distros        = []string{}
+	DistrosSet     = set.NewSet()
+	DistroPackager = map[string]string{}
+	PackagersSet   = set.NewSet(Packagers)
 )
+
+func init() {
+	for _, release := range Releases {
+		distro := strings.Split(release, "-")[0]
+		Distros = append(Distros, distro)
+		DistrosSet.Add(distro)
+	}
+
+	for _, distro := range Distros {
+		packager := ""
+
+		switch DistroPack[distro] {
+		case "debian":
+			packager = "apt"
+		case "pacman":
+			packager = "pacman"
+		case "redhat":
+			packager = "yum"
+		default:
+			panic("Failed to find packager for distro")
+		}
+
+		DistroPackager[distro] = packager
+	}
+}
