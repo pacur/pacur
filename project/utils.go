e6bb289a86ed157c4b5eae7554b49dec80599d37
Zachary Huff <zach.huff.386@gmail.com>
2015-09-06 13:59:44 -0400

    Add get targets to project

--- project/utils.go
+++ project/utils.go
@@ -1,6 +1,9 @@
 package project
 
 import (
+	"github.com/dropbox/godropbox/container/set"
+	"github.com/pacur/pacur/constants"
+	"github.com/pacur/pacur/parse"
 	"strings"
 )
 
@@ -9,6 +12,37 @@ func getDistro(name string) (distro, release string) {
 	distro = split[0]
 	if len(split) > 1 {
 		release = split[1]
+	}
+
+	return
+}
+
+func getTargets(path string) (targets []string, err error) {
+	pac, err := parse.File("", "", "/pacur")
+	if err != nil {
+		return
+	}
+
+	err = pac.Compile()
+	if err != nil {
+		return
+	}
+
+	tarSet := set.NewSet()
+	for _, target := range pac.Targets {
+		tarSet.Add(target)
+	}
+
+	for _, release := range constants.Releases {
+		distro := strings.Split(release, "-")[0]
+
+		if tarSet.Contains("!"+release) || tarSet.Contains("!"+distro) {
+			continue
+		}
+
+		if tarSet.Contains(release) || tarSet.Contains(distro) {
+			targets = append(targets, release)
+		}
 	}
 
 	return
