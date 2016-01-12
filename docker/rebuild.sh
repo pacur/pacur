80edc6dbb1f6ef5e4c7d32a4bdd27a17b7ba9dbd
Zachary Huff <zach.huff.386@gmail.com>
2016-01-11 22:16:54 -0500

    Change docker tag

--- docker/rebuild.sh
+++ docker/rebuild.sh
@@ -3,6 +3,6 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 for dir in */ ; do
     cd $dir
-    docker build --rm --no-cache -t ${dir::-1} .
+    docker build --rm --no-cache -t pacur/${dir::-1} .
     cd ..
 done
