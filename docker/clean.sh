26da6889b3971fe5182f1c857340d3cca8bb4dc8
Zachary Huff <zach.huff.386@gmail.com>
2017-11-17 05:18:23 -0500

    Add amazonlinux 2017.09

--- docker/clean.sh
+++ docker/clean.sh
@@ -3,6 +3,7 @@ docker rm $(docker ps -a -q)
 docker rmi pacur/archlinux
 docker rmi pacur/amazonlinux-2016.09
 docker rmi pacur/amazonlinux-2017.03
+docker rmi pacur/amazonlinux-2017.09
 docker rmi pacur/centos-6
 docker rmi pacur/centos-7
 docker rmi pacur/debian-jessie
@@ -25,6 +26,7 @@ docker rmi pacur/ubuntu-zesty
 docker rmi pacur/ubuntu-artful
 docker rmi amazonlinux:2016.09
 docker rmi amazonlinux:2017.03
+docker rmi amazonlinux:2017.09
 docker rmi centos:6
 docker rmi centos:7
 docker rmi debian:jessie
