7d05d1ff253169b44b596dff2a213c1318f7251d
M0Rf30 <morf3089@gmail.com>
2019-11-23 11:08:10 +0100

    cli refactoring

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -2,33 +2,101 @@ package cmd
 
 import (
 	"flag"
+	"fmt"
+	"log"
+	"os"
 
-	"github.com/dropbox/godropbox/errors"
-	"github.com/m0rf30/pacur/utils"
+	"github.com/urfave/cli"
 )
 
+// Parse retrieve cli strings and
+// return an error
 func Parse() (err error) {
 	flag.Parse()
 
 	cmd := flag.Arg(0)
-	switch cmd {
-	case "build":
-		err = Build()
-	case "create":
-		err = Create()
-	case "project":
-		err = Project()
-	case "pull":
-		err = utils.PullContainers()
-	case "genkey":
-		err = GenKey()
-	case "list-targets":
-		err = ListTargets()
-	default:
-		err = &UnknownCommand{
-			errors.Newf("cmd: Unknown command '%s'", cmd),
-		}
+
+	app := cli.NewApp()
+	app.Name = "pacur"
+	app.Usage = "make an explosive entrance"
+	app.Action = func(c *cli.Context) error {
+		fmt.Println("boom! I say!")
+		return nil
 	}
 
-	return
+	app.Commands = []cli.Command{
+		{
+			Name:    "build",
+			Aliases: []string{"a"},
+			Usage:   "add a task to the list",
+			Action: func(c *cli.Context) error {
+				fmt.Println("added task: ", c.Args().First())
+				return nil
+			},
+		},
+		{
+			Name:    "create",
+			Aliases: []string{"c"},
+			Usage:   "complete a task on the list",
+			Action: func(c *cli.Context) error {
+				fmt.Println("completed task: ", c.Args().First())
+				return nil
+			},
+		},
+		{
+			Name:    "project",
+			Aliases: []string{"t"},
+			Usage:   "options for task templates",
+			Subcommands: []cli.Command{
+				{
+					Name:  "add",
+					Usage: "add a new template",
+					Action: func(c *cli.Context) error {
+						fmt.Println("new task template: ", c.Args().First())
+						return nil
+					},
+				},
+				{
+					Name:  "remove",
+					Usage: "remove an existing template",
+					Action: func(c *cli.Context) error {
+						fmt.Println("removed task template: ", c.Args().First())
+						return nil
+					},
+				},
+			},
+		},
+		{
+			Name:    "pull",
+			Aliases: []string{"a"},
+			Usage:   "add a task to the list",
+			Action: func(c *cli.Context) error {
+				fmt.Println("added task: ", c.Args().First())
+				return nil
+			},
+		},
+		{
+			Name:    "gen-key",
+			Aliases: []string{"a"},
+			Usage:   "add a task to the list",
+			Action: func(c *cli.Context) error {
+				fmt.Println("added task: ", c.Args().First())
+				return nil
+			},
+		},
+		{
+			Name:    "list-targets",
+			Aliases: []string{"a"},
+			Usage:   "add a task to the list",
+			Action: func(c *cli.Context) error {
+				fmt.Println("added task: ", c.Args().First())
+				return nil
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
