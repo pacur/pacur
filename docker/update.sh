dea6c16b17e07cea5a371f421dd2d30cabc4924b
Корезин Артем Валерьевич <5399964+AKorezin@users.noreply.github.com>
2020-11-13 08:52:01 +0300

    Fix some potential errors in shell scripts

--- docker/update.sh
+++ docker/update.sh
@@ -2,9 +2,9 @@
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 for dir in */ ; do
-    cd $dir
-    sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur # `date`|g" Dockerfile
-    sudo docker build --rm -t pacur/${dir::-1} .
+    cd "$dir"
+    sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur # $(date)|g" Dockerfile
+    sudo docker build --rm -t "pacur/${dir::-1}" .
     sed -i -e "s|go get github.com/pacur/pacur.*|go get github.com/pacur/pacur|g" Dockerfile
     cd ..
 done
