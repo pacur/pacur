8dad42f70016df15a6aeb69df83fc00a1e64b70a
Zachary Huff <zach.huff.386@gmail.com>
2025-08-29 12:31:31 -0400

    Update rhel filenames in redhat

--- redhat/redhat.go
+++ redhat/redhat.go
@@ -138,6 +138,8 @@ func (r *Redhat) createSpec(files []string) (err error) {
 		release = ".el8.almalinux"
 	} else if r.Pack.Distro == "almalinux" && r.Pack.Release == "9" {
 		release = ".el9.almalinux"
+	} else if r.Pack.Distro == "almalinux" && r.Pack.Release == "10" {
+		release = ".el10.almalinux"
 	} else if r.Pack.Distro == "amazonlinux" && r.Pack.Release == "2" {
 		release = ".amzn2"
 	} else if r.Pack.Distro == "amazonlinux" && r.Pack.Release == "2023" {
@@ -148,10 +150,14 @@ func (r *Redhat) createSpec(files []string) (err error) {
 		release = ".el8.oraclelinux"
 	} else if r.Pack.Distro == "oraclelinux" && r.Pack.Release == "9" {
 		release = ".el9.oraclelinux"
+	} else if r.Pack.Distro == "oraclelinux" && r.Pack.Release == "10" {
+		release = ".el10.oraclelinux"
 	} else if r.Pack.Distro == "rockylinux" && r.Pack.Release == "8" {
 		release = ".el8.rockylinux"
 	} else if r.Pack.Distro == "rockylinux" && r.Pack.Release == "9" {
 		release = ".el9.rockylinux"
+	} else if r.Pack.Distro == "rockylinux" && r.Pack.Release == "10" {
+		release = ".el10.rockylinux"
 	}
 
 	data := ""
